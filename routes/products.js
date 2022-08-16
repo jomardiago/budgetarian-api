760bf2b57726b0930e8ea81bec1d0f231ed0b6cb
Jose <Jose.Diago@kyndryl.com>
2022-08-16 10:28:47 +0800

    Added update product api route.

--- routes/products.js
+++ routes/products.js
@@ -22,4 +22,16 @@ router.post('/', async (req, res) => {
   }
 });
 
+router.put('/:id', async (req, res) => {
+  try {
+    delete req.body._id;
+    await Product.findByIdAndUpdate(req.params.id, {
+      ...req.body,
+    });
+    res.status(200).json({ success: true, message: 'product successfully updated' });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
 module.exports = router;
